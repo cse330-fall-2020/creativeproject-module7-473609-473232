1e09d679f2d3f96979ab7333a6544aed3788eef6
MaxHanley <54517976+MaxHanley@users.noreply.github.com>
2020-12-11 18:44:44 -0600

    Add files via upload

--- App.js
+++ App.js
@@ -19,11 +19,32 @@ function App() {
 
         <p>The current time is {currentTime}.</p>
         <p>hello there</p>
+        <form>
+          Username: <input type = "text" id = "username" pattern = "[A-Za-z0-0]{1,20}"></input>
+          Password: <input type = "text" id = "password" pattern = "[A-Za-z0-0]{1,20}"></input>
+          <input type = "submit" id = "loginButton"></input>
+        </form>
+
       </header>
     </div>
 
   );
 
 }
+
+function signIn(email, password){
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((user) => {
+    alert("signed in")
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+}
+
+document.getElementById("loginButton").addEventListener("click",signIn(document.getElementById("username"),document.getElementById("password")),false);
+
+
 
 export default App;
